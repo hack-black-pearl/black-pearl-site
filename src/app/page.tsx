83437cf026dace9146cda400028a734581c6c1ca
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2024-11-07 14:42:59 +0530

    maintainance landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,45 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { AuroraBackground } from "./components/ui/landing-background";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-
-      </main>
-    </div>
+    <AuroraBackground>
+        <motion.div
+            initial={{ opacity: 0.0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+                delay: 0.3,
+                duration: 0.8,
+                ease: "easeInOut",
+            }}
+            className="relative flex flex-col gap-4 items-center justify-center px-4">
+                
+            <Image
+                    className="align-middle"
+                    src="/black_pearl.png"
+                    alt="Black Pearl Logo"
+                    width={500}
+                    height={50}
+                    priority/>
+            <div className="text-3xl md:text-4xl font-bold dark:text-white text-center">
+                Coming Soon...
+            </div>
+        </motion.div>
+    </AuroraBackground>
   );
 }
+
+{/* <main className="flex flex-col justify-center gap-8 row-start-2 items-center">
+                    
+
+                    <h1 className="text-4xl text-white font-bold ">Coming Soon ....</h1>
+
+                </main> */}
+
+    //             <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        
+
+    // </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,60 @@
+
 import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+const {
+    default: flattenColorPalette,
+    } = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+  /** @type {import('tailwindcss').Config} */
+  const config: Config = {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  
+      // Or if using `src` directory:
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    darkMode: "class",
+    theme: {
+      extend: {
         
+        colors: {
+          background: "var(--background)",
+          foreground: "var(--foreground)",
+        },
+        animation: {
+            aurora: "aurora 60s linear infinite",
+          },
+          keyframes: {
+            aurora: {
+              from: {
+                backgroundPosition: "50% 50%, 50% 50%",
+              },
+              to: {
+                backgroundPosition: "350% 50%, 350% 50%",
+              },
+            },
+            },
       },
     },
-  },
-  plugins: [],
-};
+    plugins: [addVariablesForColors],
+  };
+  
+  // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+  function addVariablesForColors({ addBase, theme }: any) {
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(
+      Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
+  
+    addBase({
+      ":root": newVars,
+    });
+  }
+  
+
+
+
 export default config;
